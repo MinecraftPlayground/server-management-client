06fb3677bf46fd4de871365b6a462c8ecf340f19
Mqxx <62719703+Mqxx@users.noreply.github.com>
2026-02-12 14:40:52 +0100

    Update pending_request.ts

--- src/pending_request.ts
+++ src/pending_request.ts
@@ -1,6 +1,10 @@
 import type { ErrorObject } from './json_rpc/communication/error_object.ts';
 
-
+/**
+ * Represents a pending request
+ * 
+ * @template Result Result for the resolve method
+ */
 export interface PendingRequest<Result> {
   resolve : (result : Result) => void,
   reject : (error : ErrorObject) => void
